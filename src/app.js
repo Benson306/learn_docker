4a529353f837564570ddc29fdf768dc15d0c3785
Benson Ndiwa <bnkimtai@gmail.com>
2023-09-21 11:00:00 +0300

    Welcome

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send("Welcome to my awesome app!");
+    res.send("Welcome to my  app!");
 })
 
 app.listen(3000, ()=>{
