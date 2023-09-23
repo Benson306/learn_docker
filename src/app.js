d1befe453d668abe315f087c6d50262d9814ce76
Benson Ndiwa <bnkimtai@gmail.com>
2023-09-23 11:00:00 +0300

    Welcome returned

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send("Welcome to my  app!");
+    res.send("Welcome to my  welcome app!");
 })
 
 app.listen(3000, ()=>{
